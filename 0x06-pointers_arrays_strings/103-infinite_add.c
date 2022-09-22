b14992817492c323c75cf0b89fb848b9c74f238f
Abdulwahab <wahade2004@gmail.com>
2022-09-22 07:52:37 -0700

    cd

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,47 +1,76 @@
-include "main.h"
+#include "main.h"
 
 /**
- * infinite_add - adds two numbers
- * @n1: first number
- * @n2: second number
- * @r: buffer for result
- * @size_r: buffer size
+ * infinite_add - function to add numbers and return result in pointer
  *
- * Return: address of r or 0
+ * @n1: addend 1
+ * @n2: addend 2
+ * @r: buffer for result
+ * @size_r: size of buffer
+ * Return: string pointer
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int i, j, k, l, m, n;
+	long int num1, num2, res;
+	int count = 0;
 
-	for (i = 0; n1[i]; i++)
-		;
-	for (j = 0; n2[j]; j++)
-		;
-	if (i > size_r || j > size_r)
-		return (0);
-	m = 0;
-	for (i -= 1, j -= 1, k = 0; k < size_r - 1; i--, j--, k++)
+	num1 = str_to_int(n1);
+	num2 = str_to_int(n2);
+	res = num1 + num2;
+	while (res > 0)
 	{
-		n = m;
-		if (i >= 0)
-			n += n1[i] - '0';
-		if (j >= 0)
-			n += n2[j] - '0';
-		if (i < 0 && j < 0 && n == 0)
-		{
-			break;
-		}
-		m = n / 10;
-		r[k] = n % 10 + '0';
+		count++;
+		res /= 10;
 	}
-	r[k] = '\0';
-	if (i >= 0 || j >= 0 || m)
+	if (count + 1 > size_r)
 		return (0);
-	for (k -= 1, l = 0; l < k; k--, l++)
+	else
+		return (int_to_str(num1 + num2, r, count));
+}
+
+/**
+ * str_to_int - function to convert string digits to int
+ *
+ * @str: string of numeric digits
+ * Return: int
+ */
+long int str_to_int(char *str)
+{
+	long int num = 0, i, dec = 1;
+
+	i = 1;
+	while (str[i] != '\0')
 	{
-		m = r[k];
-		r[k] = r[l];
-		r[l] = m;
+		dec *= 10;
+		i++;
 	}
-	return (r);
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		num += (str[i] - '0') * dec;
+		dec /= 10;
+	}
+	return (num);
+}
+
+/**
+ * int_to_str - function to convert int to string
+ *
+ * @num: integer to convert
+ * @str: string to write to
+ * @count: number of digits
+ * Return: pointer string
+ */
+char *int_to_str(long int num, char *str, int count)
+{
+	str[count] = '\0';
+	while (count > 0)
+	{
+		if (count == 1)
+			str[count - 1] = '0' + num;
+		else
+			str[count - 1]  = '0' + (num % 10);
+		num /= 10;
+		count--;
+	}
+	return (str);
 }
