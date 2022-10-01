1e8525bb2edf94ffb3ccda61128a185354617d54
Abdulwahab <wahade2004@gmail.com>
2022-09-30 18:59:18 -0700

    cool

---
+++ 0x09-static_libraries/_putchar.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * main- Entry Point
+ * Description: Program to print a word
+ *
+ * Return: Always the value 0 (SUCCESS)
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
